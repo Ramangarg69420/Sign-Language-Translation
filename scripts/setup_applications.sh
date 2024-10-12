cfc28a2e08670746406b3bb06fabd3153097bd05
Ramangarg69420 <ramangarg714@gmail.com>
2024-10-12 14:25:32 +0530

    Updated start_application.sh with changes

--- scripts/setup_applications.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script sets up the application
-echo "Setting up the application..."
-# Add your setup commands here
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,4 +2,4 @@
 # This script starts the application
 echo "Starting the application..."
 # Command to start your application
-
+nohup python3 main.py > /dev/null 2>&1 &
